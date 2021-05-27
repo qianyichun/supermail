537f307d01e877f2a1276c1f7f46cba890b25afe
钱宇晨 <morningrain11@outlook.com>
2021-05-27 23:06:02 +0800

    add tabbar function

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+const Home = ()=> import('../views/home/Home')
+const Cart=()=>import('../views/cart/Cart')
+const Category=()=>import('../views/category/Category')
+const Me=()=>import('../views/me/Me')
 
 Vue.use(VueRouter)
 
 const routes = [
-  
+  {
+    path:'',
+    redirect:'/home'
+  },
+  {
+    path:'/home',
+    component:Home
+  },
+  {
+    path:'/cart',
+    component:Cart
+  },
+  {
+    path:'/me',
+    component:Me
+  },
+  {
+    path:'/category',
+    component:Category    
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+<h2>首恶</h2>
+</template>
+
+<script>
+import MainTabBar from '../../components/content/maintabbar/MainTabBar'
+export default {
+    name:"Home",
+    components:{
+        MainTabBar
+    }
+}
+</script>
+
+<style>
+
+</style>
